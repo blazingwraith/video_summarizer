244bac5f4e403c2eb5bb1c4dc4699e146e54139a
blazingwraith <120934208+blazingwraith@users.noreply.github.com>
2024-04-11 19:00:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_summary(transcript):
         summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
         summary = summary + summary_text + ' '
     return summary
+    
 
 if __name__ == '__main__':
     app.run()
-
