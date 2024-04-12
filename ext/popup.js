d84b5b9a1f63e1f9d5eb2169fc1719cbd4aa0dff
Birinchi Kumar Sarma <120934208+blazingwraith@users.noreply.github.com>
2024-04-12 20:17:37 +0530

    Update popup.js

--- ext/popup.js
+++ ext/popup.js
@@ -1,7 +1,7 @@
-const btn = document.getElementById("summarise");
+const btn = document.getElementById("summarize");
 btn.addEventListener("click", function() {
     btn.disabled = true;
-    btn.innerHTML = "Summarising...";
+    btn.innerHTML = "Summarizing...";
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         var url = tabs[0].url;
         var xhr = new XMLHttpRequest();
@@ -11,7 +11,7 @@ btn.addEventListener("click", function() {
             const p = document.getElementById("output");
             p.innerHTML = text;
             btn.disabled = false;
-            btn.innerHTML = "Summarise";
+            btn.innerHTML = "Summarize";
         }
         xhr.send();
     });
