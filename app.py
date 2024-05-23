7f983ef852bcde9c6bd54f66f78263cae767d4ac
Birinchi Kumar Sarma <120934208+blazingwraith@users.noreply.github.com>
2024-05-23 09:36:44 +0530

    Update app.py
    Bullet points

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
-from transformers import pipeline
+from transformers import pipeline;
+
 
 app = Flask(__name__)
 
@@ -8,8 +9,10 @@ app = Flask(__name__)
 def summary_api():
     url = request.args.get('url', '')
     video_id = url.split('=')[1]
-    summary = get_summary(get_transcript(video_id))
-    return summary, 200
+    transcript = get_transcript(video_id)
+    summary = get_summary(transcript)
+    bullet_point_summary = format_as_bullet_points(summary)
+    return bullet_point_summary, 200
 
 def get_transcript(video_id):
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
@@ -17,13 +20,21 @@ def get_transcript(video_id):
     return transcript
 
 def get_summary(transcript):
-    summarizer = pipeline('summarization', model="stevhliu/my_awesome_billsum_model")
-    summary = ''
-    for i in range(0, (len(transcript)//1000)+1):
-        summary_text = summarizer(transcript[i*1000:(i+1)*1000])[0]['summary_text']
-        summary = summary + summary_text + ' '
+    summarizer = pipeline('summarization', model="facebook/bart-large-cnn", min_length=20, max_length=50)
+    summary = []
+    for i in range(0, (len(transcript) // 1000) + 1):
+        summary_text = summarizer(transcript[i * 1000:(i + 1) * 1000])[0]['summary_text']
+        summary.append(summary_text)
     return summary
-    
+
+def format_as_bullet_points(summary_list):
+    bullet_points = "<ul>"
+    for summary in summary_list:
+        bullet_points += f"<li>{summary}</li>"
+    bullet_points += "</ul>"
+    return bullet_points
+
+
 
 if __name__ == '__main__':
     app.run()
